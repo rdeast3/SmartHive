14518aa2b959d2bca8305386c91ae3275235f6e4
root <root@raspberrypi.(none)>
2015-06-30 01:40:51 +0000

    Reading of temperature and humidity

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# File Name: main.py
+# Author: Ronald East
 
 import RPi.GPIO as GPIO
 import weight
+import temp_humid
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,14 +13,18 @@ if __name__ == "__main__":
 	# initialize as (ADC channel, CS, clock, input to MCP, output from MCP
 	leftCorner = weight.Weight(0)
 	leftCorner.setup()
+	temperature = temp_humid.TempHumid()
 
 	while True:
+		temperature.read()
+		print "Temp:", temperature.temp
+		print "Humid:", temperature.humid
 		for i in range(5):
 			fsr = leftCorner.readADC()
 			leftCorner.addToList(fsr)
 		med = leftCorner.findMedian()
-		print("med:", med)	
-		inp = input()
+		print "med:", med	
+		inp = raw_input()
 		leftCorner.clearList()
 		if(inp == "0"):
 			GPIO.cleanup()
--- temp_humid.py
+++ temp_humid.py
@@ -1,5 +1,44 @@
+# File name: temp_humid.py
+# Author: Ronald East
+# Description: This file contains the class for the temperature and humidity sensor.
+#		It contains the initialization of the sensor and all helper functions
+#		needed to read the temperature and humidity.
+	# add more as file written
+
+# Copyright for the functions under the Adafruit_DHT module is as follows
+# 	Copyright (c) 2014 Adafruit Industries
+#	Author: Tony DiCola
+#
+#	Permission is herby granted, free of charge, to andy person obtaining a
+# copy of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EBENT SHALL THE 
+# AUTHOS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import RPi.GPIO as GPIO
+import Adafruit_DHT
 
 class TempHumid(object):
 	def __init__(self):
-		self.info = 21
+		self.type = Adafruit_DHT.DHT11
+		self.pin = 21
+		self.data = {"temp": [], "humid": []}
+		self.temp = []
+		self.humid = []
+
+	def read(self):
+		humidRead, tempRead = Adafruit_DHT.read_retry(self.type, self.pin)
+		self.temp.append(tempRead)
+		self.humid.append(humidRead)
